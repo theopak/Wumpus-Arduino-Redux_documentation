9f4bff4e789c9ec656f9202b9043fcb07b4faf1a
Theodore X. Pak <mail@theopak.com>
2012-10-30 19:18:02 -0400

    Adds Frameworks, Content, Styles. This commit adds configuration files for the hyde static site generator and skeleton site content. This commit also adds a working draft for site styling, including the source SASSY CSS files and compressed generated CSS file.

---
+++ content/assets/js/main.js
@@ -0,0 +1,23 @@
+---
+combine:
+  sort: false
+  files:
+    #- jquery-1.7.2.min.js  # via google cdn
+    - jquery.*.js
+    #- bootstrap*.js
+    - prettify.js
+  where: top
+  remove: yes
+---
+
+/**
+ * main.js
+ * Updated: 2012-07-30
+ * Author: Theo Pak
+ * Note that this file depends on jQuery > 1.6.3.
+ */
+
+
+
+	/* todo */
+
